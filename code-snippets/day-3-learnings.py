a6c33420390717be5902633e990f177d120854fa
Abhijit Dange <81798406+atomic-binary@users.noreply.github.com>
2021-10-09 17:41:37 -0400

    Update day-3-learnings.py

--- code-snippets/day-3-learnings.py
+++ code-snippets/day-3-learnings.py
@@ -118,3 +118,100 @@ if height >= 120:
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
+
+#Pizza exercise with multiple if conditions.
+# Congratulations, you've got a job at Python Pizza. 
+# Your first job is to build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
+# Small Pizza: $15
+# Medium Pizza: $20
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L \n")
+add_pepperoni = input("Do you want pepperoni? Y or N \n")
+extra_cheese = input("Do you want extra cheese? Y or N \n")
+bill = 0
+
+if size == "S":
+    bill += 15
+    print(f"Small Pizza is ${bill}")
+elif size == "M":
+    bill += 20
+    print(f"Medium Pizza is ${bill}")
+else:
+    bill += 25
+    print(f"Large Pizza is ${bill}")
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is ${bill}")
+# else:
+#     print("Please provide correct input. S, M or L.")
+# For simplicity for now, not using else here. commented above 2 lines.
+
+#LOGICAL OPERATORS
+#Multiple conditions in a single if statement with Logical Operators.
+# AND, OR, NOT
+#Multiple conditions Rollercoaster exercises.
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?\n"))
+bill = 0
+if height >= 120:
+    print("You can ride the Rollercoaster!")
+    age = int(input("What is your age?\n"))
+    if age < 12:
+        bill = 5
+        print("Children tickets are $5.")
+    elif age < 18:
+        bill = 7
+        print("Teen tickets are $7.")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("Everything is going to be ok. Have a free ride on us.!")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+    want_picture = input("Do you want to take a picture too for $3? y or n.\n")
+    if want_picture == "y":
+        bill += 3
+    print(f"Your final bill is ${bill}.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+#Love Calculator exercise.
+# You are going to write a program that tests the compatibility between two people.
+# To work out the love score between two people:
+# Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+# Then check for the number of times the letters in the word LOVE occurs. 
+# Then combine these numbers to make a 2 digit number.
+# For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is **x**, you go together like coke and mentos."
+# For Love Scores between 40 and 50, the message should be:
+# "Your score is **y**, you are alright together."
+# Otherwise, the message will just be their score. e.g.:
+# "Your score is **z**."
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+true_count = name1.count("t") + name1.count("r") + name1.count("u") + name1.count("e") + name2.count("t") + name2.count("r") + name2.count("u") + name2.count("e")
+# print(true_love_count1)
+love_count =  + name2.count("l") + name2.count("o") + name2.count("v") + name2.count("e") + name1.count("l") + name1.count("o") + name1.count("v") + name1.count("e")
+# print(true_love_count2)
+score = int(str(true_count) + str(love_count))
+# print(score)
+if score < 10 and score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
