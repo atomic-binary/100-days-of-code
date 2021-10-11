88ce22a221d3707d2db646a5c382fa99964ab68f
Abhijit Dange <81798406+atomic-binary@users.noreply.github.com>
2021-10-11 14:40:08 -0400

    Update day-4-learnings.py

--- code-snippets/day-4-learnings.py
+++ code-snippets/day-4-learnings.py
@@ -13,3 +13,20 @@ if random_number == 1:
     print("Heads")
 else:
     print("Tails")
+
+#LISTS
+#We can store mixed data types in a list as well.
+fruits = ["Cherry", "Apple", "Grape"]
+
+states_of_india = ["Maharashtra", "Karnataka", "Madhyapradesh", "Uttarakhand"]
+print(states_of_india)
+print(states_of_india[0])
+print(states_of_india[-1])
+
+states_of_india[2] = "Himachal Pradesh" #alter any existing item in the list and it will be replaced.
+states_of_india.append("Kerala")    #append() will add items to the list at the end as the word suggests.
+print(states_of_india)
+
+states_of_india.extend(["Goa", "West Bengal", "Punjab"])
+#another list can be added with extend(), this will be added to the end of the list.
+print(states_of_india)
