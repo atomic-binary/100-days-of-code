d94007f310037c555c5ea5b23b6c07323cd98a1f
Abhijit Dange <81798406+atomic-binary@users.noreply.github.com>
2021-10-11 14:23:31 -0400

    Update day-4-learnings.py

--- code-snippets/day-4-learnings.py
+++ code-snippets/day-4-learnings.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+import random
+
+# random_integer = random.randint(1, 20)
+# print(random_integer)
+# random_float = random.random()
+# print(random_float)
+
+#Heads/Tails exercise.
+random_number = random.randint(0, 1)
+if random_number == 1:
+    print("Heads")
+else:
+    print("Tails")
